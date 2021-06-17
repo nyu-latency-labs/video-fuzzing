27f2caff6139d9c052cab8872127cc15017bf77f
Vinayak Agarwal <va2083@nyu.edu>
2021-06-17 16:10:47 -0400

    Add classes to make script generic

--- compositor/moviepy/moviegen.py
+++ compositor/moviepy/moviegen.py
@@ -1,22 +1,93 @@
 # Import everything needed to edit video clips
 from moviepy.editor import *
 
-# Load myHolidays.mp4 and select the subclip 00:00:50 - 00:00:60
-imageclip = ImageClip("./resources/street.jpg").resize((500,500))
+class VideoState:
+    location = "../resources/cars/1.mp4"
+    offset = 0
+    dim_x = 400
+    dim_y = 200
+    pos_x = 0
+    pos_y = 0
+    duration_played = 0
+    duration_step = 0
 
-clip = VideoFileClip("./resources/cars/1.mp4").resize((100,100))
-clip = clip.set_position((0,0)).set_end(5)
+    def __init__(self, location, offset, pos_x, pos_y, dim_x, dim_y, duration_step):
+        self.location = location
+        self.offset = offset
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.dim_x = dim_x
+        self.dim_y = dim_y
+        self.duration_played = offset
+        self.duration_step = duration_step
 
-clip1 = VideoFileClip("./resources/cars/1.mp4").subclip(5,15).resize((100,100))
-clip1 = clip1.set_position((100,0))
+    def getNextStepClip(self, start_time):
+        clip = VideoFileClip(self.location).resize((self.dim_x,self.dim_y)).set_position((self.pos_x, self.pos_y)) \
+            .subclip(self.duration_played, self.duration_played + self.duration_step).set_start(start_time) \
+            .set_end(start_time+self.duration_step)
+        return clip
 
-clip2 = VideoFileClip("./resources/cars/1.mp4").subclip(10,20).resize((100,100))
-clip2 = clip2.set_position((200,0))
+class Props:
+    duration = 10
+    duration_step = 2
+    grid_x = 2
+    grid_y = 2
+    dim_x = 400
+    dim_y = 200
+    grid_occupied = None
+    size_x = 200
+    size_y = 100
 
-clip3 = VideoFileClip("./resources/cars/2.mov").subclip(0,20).resize((100,100))
-clip3 = clip3.set_position((0,0)).set_opacity(0.5)
-# Overlay the text clip on the first video clip
-video = CompositeVideoClip([imageclip, clip, clip1.set_start(5), clip2, clip3.set_start(5)], size=(500,500))
+    def __init__(self, duration, duration_step, grid_x, grid_y, dim_x, dim_y):
+        self.duration = duration
+        self.duration_step = duration_step
+        self.grid_x = grid_x
+        self.grid_y = grid_y
+        self.dim_x = dim_x
+        self.dim_y = dim_y
+        self.grid_occupied = [ [False]*grid_y for i in range(grid_x)]
+        self.size_x = dim_x/grid_x
+        self.size_y = dim_y/grid_y
 
-# Write the result to a file (many options available !)
-video.set_duration(10).write_videofile("output.mp4",fps=30,bitrate="1000k",audio_codec="mp3",codec="mpeg4")
+    def allocatePosition(self):
+        for i in range(self.grid_x):
+            for j in range(self.grid_y):
+                if ( not self.grid_occupied[i][j]):
+                    self.grid_occupied[i][j] = True
+                    return (i*self.size_x, j*self.size_y)
+        return (-1,-1)
+
+    def freePosition(self, pos_x, pos_y):
+        self.grid_occupied[pos_x][pos_y] = False
+
+    def getVideoSize(self):
+        return (self.size_x, self.size_y)
+
+
+props = Props(10, 2, 2, 2, 400, 200)
+ 
+location1 = "../resources/cars/1.mp4"
+location2 = "../resources/cars/3.mp4"   
+location3 = "../resources/cars/4.mp4"
+location4 = "../resources/cars/2.mov"
+
+(dim_x, dim_y) = props.getVideoSize()
+
+(pos_x, pos_y) = props.allocatePosition()
+video1 = VideoState(location1, 0, pos_x, pos_y, dim_x, dim_y, props.duration_step)
+
+(pos_x, pos_y) = props.allocatePosition()
+video2 = VideoState(location2, 0, pos_x, pos_y, dim_x, dim_y, props.duration_step)
+
+(pos_x, pos_y) = props.allocatePosition()
+video3 = VideoState(location3, 0, pos_x, pos_y, dim_x, dim_y, props.duration_step)
+
+(pos_x, pos_y) = props.allocatePosition()
+video4 = VideoState(location4, 0, pos_x, pos_y, dim_x, dim_y, props.duration_step)
+
+imageclip = ImageClip("../resources/street.jpg").resize((400,200))
+
+video = CompositeVideoClip([imageclip, video1.getNextStepClip(0), video2.getNextStepClip(1), video3.getNextStepClip(4), 
+video4.getNextStepClip(6)])
+
+video.set_duration(10).write_videofile("output.mp4",fps=25,bitrate="500k",audio_codec=None,codec="mpeg4")
