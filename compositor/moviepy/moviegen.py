c84f55e4878918af61f3d79b28a2c60dbf7567c8
Vinayak Agarwal <va2083@nyu.edu>
2021-06-18 15:35:36 -0400

    Add XY class for simplicity

--- compositor/moviepy/moviegen.py
+++ compositor/moviepy/moviegen.py
@@ -3,6 +3,17 @@ from moviepy.editor import *
 import random
 import math
 
+class XY:
+    x = 0
+    y = 0
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def getXY(self):
+        return (self.x, self.y)
+
 class VideoState:
     location = "../resources/cars/1.mp4"
     offset = 0
@@ -15,16 +26,14 @@ class VideoState:
     clip = None
     clip_duration = 0
 
-    def __init__(self, location, offset, pos_x, pos_y, dim_x, dim_y, duration_step):
+    def __init__(self, location, offset, pos: XY, dim: XY, duration_step):
         self.location = location
         self.offset = offset
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.dim_x = dim_x
-        self.dim_y = dim_y
+        self.pos = pos
+        self.dim = dim
         self.duration_played = offset
         self.duration_step = duration_step
-        self.clip = VideoFileClip(self.location).resize((self.dim_x,self.dim_y)).set_position((self.pos_x, self.pos_y))
+        self.clip = VideoFileClip(self.location).resize(dim.getXY()).set_position(pos.getXY())
         self.clip_duration = self.clip.duration
 
     def getNextStepClip(self, start_time):
@@ -38,64 +47,57 @@ class VideoState:
 class Props:
     duration = 10
     duration_step = 2
-    grid_x = 2
-    grid_y = 2
-    dim_x = 400
-    dim_y = 200
+    grid = None
+    dim = None
     grid_occupied = None
-    size_x = 200
-    size_y = 100
+    size = None
 
-    def __init__(self, duration, duration_step, grid_x, grid_y, dim_x, dim_y):
+    def __init__(self, duration, duration_step, grid: XY, dim: XY):
         self.duration = duration
         self.duration_step = duration_step
-        self.grid_x = grid_x
-        self.grid_y = grid_y
-        self.dim_x = dim_x
-        self.dim_y = dim_y
-        self.grid_occupied = [ [False]*grid_y for i in range(grid_x)]
-        self.size_x = dim_x/grid_x
-        self.size_y = dim_y/grid_y
+        self.grid = grid
+        self.dim = dim
+        self.grid_occupied = [ [False]*grid.y for i in range(grid.x)]
+        self.size = XY(dim.x/grid.x, dim.y/grid.y)
 
     def allocatePosition(self):
-        for i in range(self.grid_x):
-            for j in range(self.grid_y):
+        for i in range(self.grid.x):
+            for j in range(self.grid.y):
                 if ( not self.grid_occupied[i][j]):
                     self.grid_occupied[i][j] = True
-                    return (i*self.size_x, j*self.size_y)
-        return (-1,-1)
+                    return XY(i*self.size.x, j*self.size.y)
+        return XY(-1,-1)
 
-    def freePosition(self, pos_x, pos_y):
-        self.grid_occupied[pos_x][pos_y] = False
+    def freePosition(self, pos: XY):
+        self.grid_occupied[pos.x][pos.y] = False
 
     def getVideoSize(self):
-        return (self.size_x, self.size_y)
+        return XY(self.size.x, self.size.y)
 
 
-props = Props(40, 2, 2, 2, 400, 200)
+props = Props(40, 2, XY(2, 2), XY(400, 200))
  
 location1 = "../resources/cars/1.mp4"
 location2 = "../resources/cars/3.mp4"   
 location3 = "../resources/cars/4.mp4"
 location4 = "../resources/cars/2.mov"
 
-(dim_x, dim_y) = props.getVideoSize()
+dim = props.getVideoSize()
 videos = []
 
-(pos_x, pos_y) = props.allocatePosition()
-videos.append(VideoState(location1, 0, pos_x, pos_y, dim_x, dim_y, props.duration_step))
+pos = props.allocatePosition()
+videos.append(VideoState(location1, 0, pos, dim, props.duration_step))
 
-(pos_x, pos_y) = props.allocatePosition()
-videos.append(VideoState(location2, 0, pos_x, pos_y, dim_x, dim_y, props.duration_step))
+pos = props.allocatePosition()
+videos.append(VideoState(location2, 0, pos, dim, props.duration_step))
 
-(pos_x, pos_y) = props.allocatePosition()
-videos.append(VideoState(location3, 0, pos_x, pos_y, dim_x, dim_y, props.duration_step))
+pos = props.allocatePosition()
+videos.append(VideoState(location3, 0, pos, dim, props.duration_step))
 
-(pos_x, pos_y) = props.allocatePosition()
-videos.append(VideoState(location4, 0, pos_x, pos_y, dim_x, dim_y, props.duration_step))
+pos = props.allocatePosition()
+videos.append(VideoState(location4, 0, pos, dim, props.duration_step))
 
-imageclip = ImageClip("../resources/street.jpg").resize((dim_x, dim_y))
-
+imageclip = ImageClip("../resources/street.jpg").resize(dim.getXY())
 
 distribution = []
 for i in range(20):
@@ -108,6 +110,6 @@ for i in range(20):
     for j in range(round(distribution[i])):
         clips.append(videos[j].getNextStepClip(i*2))
 
-video = CompositeVideoClip(clips).set_audio(None).resize((dim_x, dim_y))
+video = CompositeVideoClip(clips).set_audio(None).resize(dim.getXY())
 
 video.set_duration(40).write_videofile("output.mp4",fps=25,bitrate="500k",audio_codec=None,codec="mpeg4")
