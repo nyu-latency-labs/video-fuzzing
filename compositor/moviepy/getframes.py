2ff8c7cd1bd75292ddd57589f0f4cab7ee00d274
Vinayak Agarwal <va2083@nyu.edu>
2021-08-03 10:47:48 -0400

    Modify getframes to generate folder name based on filename

--- compositor/moviepy/getframes.py
+++ compositor/moviepy/getframes.py
@@ -4,6 +4,7 @@ import sys
 
 DURATION_STEP = 2
 path = sys.argv[1]
+base_name = os.path.basename(path).split(".")[0]
 
 video = VideoFileClip(path, audio=False)
 frames = []
@@ -13,7 +14,7 @@ Path("output_frames/").mkdir(parents=True, exist_ok=True)
 
 for i in range(int(DURATION_STEP/2), int(video.duration), DURATION_STEP):
     frame = video.get_frame(i)
-    video.save_frame("output_frames/frame_" + str(i) + ".png", t = i)
+    video.save_frame(base_name + "_frames/frame_" + str(i) + ".png", t = i)
 
 
 
